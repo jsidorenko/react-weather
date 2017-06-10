fb918ed247270b4f8898e59281a4f13a773c478f
Jegor Sidorenko <jegor@nfm.lt>
2017-06-10 17:16:00 +0100

    Basic weather app

---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+/*var WeatherMessage = React.createClass({
+  render: function () {
+    var {temp, location} = this.props;
+
+    return (
+      <div>It is {temp} degree in {location}</div>
+    );
+  }
+});*/
+
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <div>It is {temp} degree in {location}</div>
+  );
+};
+
+module.exports = WeatherMessage;
